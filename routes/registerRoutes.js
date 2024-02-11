8f62205efaf6e77d6073d1d78e347739c7183cd6
Ana Almeida <anapalmeidaj1@gmail.com>
2024-02-10 23:30:45 -0300

    feat: creating register page

---
+++ routes/registerRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const router = express.Router();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+router.get("/", (req, res, next) => {
+  var payload = {
+    pageTitle: "Create an account",
+  };
+
+  res.status(200).render("register", payload);
+});
+
+module.exports = router;
