80e547d4c2718be67bc5ff5cef07708e6f403183
Ana Almeida <anapalmeidaj1@gmail.com>
2024-02-10 21:42:00 -0300

    feat: setting up main layout structure

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.set("view engine", "pug");
 app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-  res.status(200).render("home");
+  var payload = {
+    pageTitle: "Twitter",
+  };
+
+  res.status(200).render("home", payload);
 });
