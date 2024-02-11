a18ed3797120b5b21e4ee4b31a626b0de3e84d93
Ana Almeida <anapalmeidaj1@gmail.com>
2024-02-11 16:29:10 -0300

    feat: inserting user inside cluster

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,4 +1,6 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
+const User = require("../schemas/UserSchema");
 
 const app = express();
 const router = express.Router();
@@ -20,7 +22,7 @@ router.get("/", (req, res, next) => {
   res.status(200).render("register", payload);
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   var firstName = req.body.firstName.trim();
   var lastName = req.body.lastName.trim();
   var email = req.body.email.trim();
@@ -30,6 +32,28 @@ router.post("/", (req, res, next) => {
   var payload = req.body;
 
   if (firstName && lastName && username && email && password) {
+    var user = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    }).catch((err) => {
+      console.log(err);
+      payload.errorMessage = "Something went wrong.";
+      res.status(200).render("register", payload);
+    });
+
+    if (user === null) {
+      var data = req.body;
+      data.password = await bcrypt.hash(password, 10);
+      User.create(data).then((user) => {
+        console.log(user);
+      });
+    } else {
+      if (email === user.email) {
+        payload.errorMessage = "Email already in use.";
+      } else {
+        payload.errorMessage = "Username already in use.";
+      }
+      res.status(200).render("register", payload);
+    }
   } else {
     payload.errorMessage = "Make sure each field has a valie value.";
     res.status(200).render("register", payload);
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -2,37 +2,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "/images/profilePic.png",
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-    default: "/images/profilePic.png",
-  },
-});
+  { timestamps: true }
+);
 
 var User = mongoose.model("User", UserSchema);
 module.exports = User;
